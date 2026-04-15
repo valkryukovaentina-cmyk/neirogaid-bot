03ed91a30acbaf19e6705390f2f443c51b8e8517
valkryukovaentina-cmyk <valkryukovaentina@gmail.com>
2026-04-15 20:10:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# bot code here
+import telebot
+import requests
+
+TOKEN = "AAHMVuHZzBgruVhOJifjiXy4c_Pzf8dUABI"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Нажми чтобы оплатить /buy")
+
+@bot.message_handler(commands=['buy'])
+def buy(message):
+    bot.send_message(message.chat.id, "Оплати по ссылке и нажми /paid")
+
+@bot.message_handler(commands=['paid'])
+def paid(message):
+    bot.send_message(message.chat.id, "Спасибо за оплату! Вот твой гайд 👇")
+    doc = open('guide.pdf', 'rb')
+    bot.send_document(message.chat.id, doc)
+
+bot.polling()
